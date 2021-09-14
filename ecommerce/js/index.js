547e225bdb1c6aa602bfe5769bc8293238cd210f
tlboone0074 <59076573+tlboone0074@users.noreply.github.com>
2021-09-14 13:12:25 -0400

    Add files via upload

---
+++ ecommerce/js/index.js
@@ -0,0 +1,85 @@
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
+});
+
+// Fixed Nav
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  if (!link) return;
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
+
+// reference the images on main screen and the smooth animation
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: 200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+
+
+ // ---------------------javascript for toggle form
+
+//  var LoginForm = document.getElemenyById("LoginForm")
+//  var RegForm = document.getElementById("RegForm")
+//  var Indicator = document.getElementById("RegForm")
+
+//  function register(){
+//    RegForm.style.transform = "translateX(0px)";
+//    LoginForm.style.transform = "translateX(0px)";
+//    Indicator.style.transform = "translateX(100px)";
+//  }
+
+//  function login(){
+//    RegForm.style.transform = "translateX(300px)";
+//    LoginForm.style.transform = "translateX(300px)";
+//    Indicator.style.transform = "translateX(300px)";
+//  }
+
